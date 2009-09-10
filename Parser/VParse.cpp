a5f2c1a635a6686befef30cf93304ebdef92cba7
Wilson Snyder <wsnyder@wsnyder.org>
2009-09-10 13:49:30 +0000

    Import of WSNYDER/Verilog-Perl-3.213 from CPAN.
    gitpan-cpan-distribution: Verilog-Perl
    gitpan-cpan-version:      3.213
    gitpan-cpan-path:         WSNYDER/Verilog-Perl-3.213.tar.gz
    gitpan-cpan-author:       WSNYDER
    gitpan-cpan-maturity:     released

--- Parser/VParse.cpp
+++ Parser/VParse.cpp
@@ -102,10 +102,9 @@ void VParse::setEof() {
 
 void VParse::fakeBison() {
     // Verilog::Parser and we don't care about the syntax, so just Lex.
-    static const size_t chunk = 8192;
     VParseBisonYYSType yylval;
-    char buffer[chunk+10];
     while (int tok = lexToBison(&yylval)) {
+	if (tok) {} // Prevent unused on some GCCs
     }
 }
 
